c65e678f0f65a46c6c2050abd482740f54e80524
Diego Fornalha <diegofornalha@gmail.com>
2024-12-14 06:17:25 -0300

    problema resolvido

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
-
-export default clerkMiddleware(async (auth, req) => {
-    const { userId, redirectToSignIn } = auth();
-
-    // If the user isn't signed in and the route is private, redirect to sign-in
-    if (!userId && isProtectedRoute(req)) {
-        return NextResponse.redirect(new URL('/sign-in', req.url));
-    }
-
-    // If the user is logged in and the route is protected, let them view.
-    if (userId && isProtectedRoute(req)) {
-        return NextResponse.next();
-    }
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)"],
+  ignoredRoutes: ["/api/webhooks(.*)", "/api/public(.*)"]
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
